3c27e6b8d762c97cf747ac806fde67d5747e6d33
Ram <ramprasathit@gmail.com>
2016-12-11 19:14:11 -0600

    New file

---
+++ DoIT/GA.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoIT
+{
+    class GA
+    {
+        public string Name { get; set; }
+        public string Dept { get; set; }
+    }
+}
